a8a70af9d7df762b5e777ef9da3283ce1ac77167
Ladpham <96309248+Ladpham@users.noreply.github.com>
2025-04-20 23:31:41 +0000

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Client } from 'boardgame.io/react';
+import { SocketIO } from 'boardgame.io/multiplayer';
+import { Backgammon } from './Game';
+import Board from './Board';
+import './index.css';
+
+const App = Client({
+  game: Backgammon,
+  board: Board,
+  numPlayers: 2,
+  debug: false,
+  multiplayer: SocketIO(),          // uses same origin
+  setupData: { matchID: 'TIB', target: 5 },
+});
+
+ReactDOM.render(
+  <div>
+    <header>
+      <img src="/tib-logo.png" alt="TIB Logo" />
+      <h1>TIB Backgammon</h1>
+    </header>
+    <App playerID="0" matchID="TIB" />
+  </div>,
+  document.getElementById('root')
+);
