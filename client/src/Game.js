c3bb3b8916105c42d786e80d39cb40141c140467
Ladpham <96309248+Ladpham@users.noreply.github.com>
2025-04-21 00:02:28 +0000

    Update Game.js

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,2 +1,1 @@
-// Use boardgame.io’s built-in Backgammon game logic (includes PR & Luck)
-export { Backgammon } from 'boardgame.io/examples/src/games/backgammon';
+export { Backgammon } from './vendor/backgammon';
